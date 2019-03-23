5da9dac4416f61803d0b924f7a2e59e1e0c4c03f
Hoang Tran <eaglegold125@gmail.com>
2019-03-23 15:20:26 +0700

    update

--- Front-end/data/data.js
+++ Front-end/data/data.js
@@ -1,9 +1,117 @@
 var programmingBooks = [
     {
-        name: "",
+        name: "The C++ Programming Language, 4th Edition 4th Edition",
+        image: "https://images-na.ssl-images-amazon.com/images/I/51KEqIsBa4L._SX370_BO1,204,203,200_.jpg",
+        description: "The new C++11 standard allows programmers to express ideas more clearly, simply, and directly, and to write faster, more efficient code. Bjarne Stroustrup, the designer and original implementer of C++, has reorganized, extended, and completely rewritten his definitive reference and tutorial for programmers who want to use C++ most effectively.",
+        price: 50.77,
+        author: "Bjarne Stroustrup",
+    },
+    {
+        name: "Effective Java 3rd Edition",
+        image: "https://images-na.ssl-images-amazon.com/images/I/516yIUucShL._SX408_BO1,204,203,200_.jpg",
+        description: "The Definitive Guide to Java Platform Best Practices–Updated for Java 7, 8, and 9 Java has changed dramatically since the previous edition of Effective Java was published shortly after the release of Java 6. This Jolt award-winning classic has now been thoroughly updated to take full advantage of the latest language and library features. The support in modern Java for multiple paradigms increases the need for specific best-practices advice, and this book delivers.",
+        price: 31.72,
+        author: "Joshua Bloch",
+    },
+    {
+        name: "Web Design with HTML, CSS, JavaScript and jQuery Set 1st Edition",
+        image: "https://images-na.ssl-images-amazon.com/images/I/41T53nRtyoL._SX435_BO1,204,203,200_.jpg",
+        description: "This two-book set combines the titles HTML & CSS: Designing and Building Web Sites and JavaScript & jQuery: Interactive Front-End Development. Together these two books form an ideal platform for anyone who wants to master HTML and CSS before stepping up to JavaScript and jQuery.",
+        price: 55.87,
+        author: "Jon Duckett",
+    },
+    {
+        name: "Introduction to Algorithms, 3rd Edition (The MIT Press) 3rd Edition",
+        image: "https://images-na.ssl-images-amazon.com/images/I/41-1VkO%2B1lL._SX359_BO1,204,203,200_.jpg",
+        description: "Some books on algorithms are rigorous but incomplete; others cover masses of material but lack rigor. Introduction to Algorithms uniquely combines rigor and comprehensiveness. The book covers a broad range of algorithms in depth, yet makes their design and analysis accessible to all levels of readers. Each chapter is relatively self-contained and can be used as a unit of study. The algorithms are described in English and in a pseudocode designed to be readable by anyone who has done a little programming. The explanations have been kept elementary without sacrificing depth of coverage or mathematical rigor.",
+        price: 79.99,
+        author: "Thomas H. Cormen",
+    },
+    {
+        name: "Design Patterns: Elements of Reusable Object-Oriented Software 1st Edition",
+        image: "https://images-na.ssl-images-amazon.com/images/I/51szD9HC9pL._SX395_BO1,204,203,200_.jpg",
+        description: "Capturing a wealth of experience about the design of object-oriented software, four top-notch designers present a catalog of simple and succinct solutions to commonly occurring design problems. Previously undocumented, these 23 patterns allow designers to create more flexible, elegant, and ultimately reusable designs without having to rediscover the design solutions themselves.",
+        price: 37.67,
+        author: "Erich Gamma",
+    },
+    {
+        name: "C# 7.0 in a Nutshell: The Definitive Reference 1st Edition",
+        image: "https://images-na.ssl-images-amazon.com/images/I/51CNw5edW1L._SX331_BO1,204,203,200_.jpg",
+        description: "When you have questions about C# 7.0 or the .NET CLR and its core Framework assemblies, this bestselling guide has the answers you need. Since its debut in 2000, C# has become a language of unusual flexibility and breadth, but its continual growth means there’s always more to learn.",
+        price: 39.49,
+        author: "Joseph Albahari",
+    },
+    {
+        name: "C Programming Language, 2nd Edition 2nd Edition",
+        image: "https://images-na.ssl-images-amazon.com/images/I/41gHB8KelXL._SX377_BO1,204,203,200_.jpg",
+        description: "The authors present the complete guide to ANSI standard C language programming. Written by the developers of C, this new version helps readers keep up with the finalized ANSI standard for C while showing how to take advantage of C's rich set of operators, economy of expression, improved control flow, and data structures. The 2/E has been completely rewritten with additional examples and problem sets to clarify the implementation of difficult language constructs. For years, C programmers have let K&R guide them to building well-structured and efficient programs. Now this same help is available to those working with ANSI compilers. Includes detailed coverage of the C language plus the official C language reference manual for at-a-glance help with syntax notation, declarations, ANSI changes, scope rules, and the list goes on and on.",
+        price: 41.16,
+        author: "Brian W.Kernighan",
+    },
+    {
+        name: "Cracking the Coding Interview: 189 Programming Questions and Solutions 6th Edition",
+        image: "https://images-na.ssl-images-amazon.com/images/I/51l5XzLln%2BL._SX348_BO1,204,203,200_.jpg",
+        description: "Cracking the Coding Interview, 6th Edition is here to help you through this process, teaching you what you need to know and enabling you to perform at your very best. I've coached and interviewed hundreds of software engineers. The result is this book.",
+        price: 32.00,
+        author: "Gayle Laakmann McDowell",
+    },
+];
+
+var businessBooks = [
+    {
+        name: "Gameplan: The Complete Strategy Guide to go from Starter Kit to Silver",
+        image: "https://images-na.ssl-images-amazon.com/images/I/41DF33qVDHL.jpg",
+        description: "With a passion for helping others and a modern approach to sharing the oils, Sarah Harnisch made it to Platinum in Young Living in just 17 months. Now she’s sharing her strategy in this densely-packed guide to take you from starter kit to Silver in Young Living.",
+        price: 18.94,
+        author: "Sarah Harnisch"
+    },
+    {
+        name: "The Ambitious Woman: What It Takes and Why You Want to Be One",
+        image: "https://images-na.ssl-images-amazon.com/images/I/51MmUYC7qLL._SX324_BO1,204,203,200_.jpg",
+        description: "The Ambitious Woman illustrates how ambitious women embody characteristics such as inspiration, empowerment, and motivation to help others accomplish their goals, and create win-win relationships in all areas of life. When you think of the word ‘ambitious’, what comes to mind? Power hungry? Getting ahead no matter what it takes? Someone who runs over others to accomplish their goals? While many people may have a negative view of what it means to be ambitious, author, motivational speaker and women’s movement leader, Esther Spina, is not one of them.",
+        price: 17.16,
+        author: "Esther Spina",
+    },
+    {
+        name: "The Front Row Factor: Transform Your Life with the Art of Moment Making",
+        image: "https://images-na.ssl-images-amazon.com/images/I/51l5C4lbTHL.jpg",
+        description: "It helps readers cultivate an empowering mindset, create life-long relationships and design an environment where you can thrive regardless of life conditions. ",
+        price: 14.88,
+        author: "Jon Vroman",
+    },
+    {
+        name: "Freelance to Freedom: The Roadmap for Creating a Side Business to Achieve Financial, Time and Life Freedom",
+        image: "https://images-na.ssl-images-amazon.com/images/I/51RlKvPTrJL._SX331_BO1,204,203,200_.jpg",
+        description: "Vincent and his wife were stuck in dead end newspaper photography jobs, in debt, stressed, with a baby on the way while making $15 an hour. After winning the highest award in his field, Vincent was offered a 3 percent raise. He knew at that moment he needed a monumental change. One month away from their baby being born, Vincent and Elizabeth started a side photography business out of desperation. In less than four years, they grew their business to pay off all of their debt, including their home, and left their jobs for a life of freedom. With the world moving rapidly towards a freelance model, Freelance to Freedom is not only timely and necessary, but it’s also entertaining, engaging and paints a picture for anyone looking for a life of freedom with money, time and location.",
+        price: 12.20,
+        author: "Vincent Pugliese",
+    },
+    {
+        name: "Best Ever Apartment Syndication Book",
+        image: "https://images-na.ssl-images-amazon.com/images/I/51kDcz-6R%2BL._SX331_BO1,204,203,200_.jpg",
+        description: "Do you want to purchase an apartment community but don't have prior real estate or apartment investing experience, don't have access to private capital, can't find a good deal, lack the skills to execute a business plan or a combination of the four?",
+        price: 53.80,
+        author: "Joe Fairless"
+    },
+    {
+        name: "Leadershift: The 11 Essential Changes Every Leader Must Embrace",
+        image: "https://images-na.ssl-images-amazon.com/images/I/411XVg0bP3L._SX332_BO1,204,203,200_.jpg",
+        description: "Internationally recognized leadership expert John C. Maxwell teaches readers how to shift their leadership to keep innovating, improving, and influencing others in today’s fast-paced world.",
+        price: 14.99,
+        author: "John C. Maxwell",
+    },
+    {
+        name: "Power Up: How Smart Women Win in the New Economy",
+        image: "https://images-na.ssl-images-amazon.com/images/I/410BXK0OUJL._SX332_BO1,204,203,200_.jpg",
+        description: "With empowering insights to help women navigate the narrowest corridors of sexism, tech-industry pioneer Magdalena Yesil shares on-the-ground career advice that is as powerful as any MBA",
+        price: 17.70,
+        author: "Magdalena Yesil",
+    },
+    {
+        name: "Growing Weeders Into Leaders: Leadership Lessons from the Ground Up",
         image: "",
-        description: "",
-        price: "",
-        author: ""
+        description: "It is an entertaining and thoughtful look into the hearts and the workday lives of ordinary people - just like you and me - who tapped into their inner greatness in pursuit of a vision. Creating one of America's most beautiful college campuses, at Ole Miss, did not happen overnight and, inside these pages, Jeff McManus describes the joys, the defeats, the brilliant problem-solving and the 'best laid plans' that are proven worthless...until the bigger picture is told. It is the 'bigger picture' told from the ground level. Growing Weeders into Leaders takes you through the practical applications of empowering people to experience not only what it means to grow outstanding landscapes, but also to grow greatness in themselves and encourage it in others.",
+        price: 11.52,
+        author: "Jeff McManus",
     }
-]
+];
